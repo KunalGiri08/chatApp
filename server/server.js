1c5b8bf3887be4cc19c70576eb98ab59a8e4adde
Kunal Giri <kunalgiri177@gmail.com>
2026-05-28 16:19:30 +0530

    Initialize server with Express, MongoDB connection, and basic API setup

---
+++ server/server.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import http from 'http';
+import 'dotenv/config';
+import cors from 'cors';
+import { connectDB } from './lib/db.js';
+
+import dns from 'node:dns';
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+
+// Create Express app and HTTP server
+
+const app = express();
+
+const server = http.createServer(app)
+
+
+// Middleware setup
+
+app.use(express.json({limit: "4mb"}));
+
+app.use(cors());
+
+
+app.use("/api/status", (req, res)=> res.send("Server is live"));
+
+// Connect to the database
+
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, ()=> console.log("Server is running on PORT: " + PORT));
