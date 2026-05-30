3b57945a4fb4dcef7e7516b5ca8842a2ea6ec764
Kunal Giri <kunalgiri177@gmail.com>
2026-05-30 10:46:50 +0530

    Implement ChatContext for messaging functionality and update Sidebar and HomePage components using backend features

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
-import assets ,{ userDummyData} from '../assets/assets'
+import React, { useState,useEffect } from 'react'
+import assets from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
 import { useContext } from 'react'
 import { AuthContext } from '../../context/AuthContext'
+import { ChatContext } from '../../context/ChatContext'
 
-const Sidebar = ({ selectedUser, setSelectedUser }) => {
-    const {logout} = useContext(AuthContext)
+const Sidebar = () => {
+
+    const { getUsers, users, selectedUser, setSelectedUser,
+        unseenMessages, setUnseenMessages } = useContext(ChatContext);
+
+    const { logout, onlineUsers } = useContext(AuthContext);
+    const [input, setInput] = useState(false);
 
     const navigate = useNavigate();
+
+    const filteredUsers = input
+        ? users.filter((user) =>
+            user.fullName.toLowerCase().includes(input.toLowerCase())
+        )
+        : users;
+
+    useEffect(() => {
+        getUsers();
+    }, [onlineUsers])
+
 
     return (
         <div className={`bg-[#8185B2]/10 h-full p-5 rounded-r-xl overflow-y-scroll
@@ -32,7 +49,7 @@ group-hover:block'>
 
                             <hr className="my-2 border-t border-gray-500" />
 
-                            <p  onClick={() => logout()} className='cursor-pointer text-sm'>Logout</p>
+                            <p onClick={() => logout()} className='cursor-pointer text-sm'>Logout</p>
 
                         </div>
 
@@ -44,7 +61,7 @@ mt-5'>
 
                     <img src={assets.search_icon} alt="Search" className='w-3' />
 
-                    <input type="text" className='bg-transparent border-none outline-none
+                    <input onChange={(e) => setInput(e.target.value)} type="text" className='bg-transparent border-none outline-none
     text-white text-xs placeholder-[#c8c8c8] flex-1' placeholder='Search
     User...' />
 
@@ -53,7 +70,7 @@ mt-5'>
             </div>
             <div className='flex flex-col'>
 
-                {userDummyData.map((user, index) => (
+                {filteredUsers.map((user, index) => (
 
                     <div onClick={() => { setSelectedUser(user) }}
                         key={index} className={`relative flex items-center gap-2 p-2 pl-4
@@ -69,7 +86,7 @@ mt-5'>
 
                             {
 
-                                index < 3
+                                onlineUsers.includes(user._id)
 
                                     ? <span className='text-green-400 text-xs'>Online</span>
 
@@ -79,10 +96,10 @@ mt-5'>
 
                         </div>
 
-                        {index > 2 && <p className='absolute top-4 right-4 text-xs h-5 w-5
+                        {unseenMessages[user._id] > 0 && <p className='absolute top-4 right-4 text-xs h-5 w-5
 flex justify-center items-center rounded-full bg-violet-500/50'>
 
-                            {index}
+                            {unseenMessages[user._id]}
 
                         </p>}
 
