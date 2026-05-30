c33dbd57e6538aa8510038fe22e3a93ededb3a23
Kunal Giri <kunalgiri177@gmail.com>
2026-05-30 17:00:09 +0530

    Update CORS configuration to include additional origin for socket.io

--- server/server.js
+++ server/server.js
@@ -19,7 +19,8 @@ export const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173",
-      "https://chat-app-pearl-six.vercel.app"
+      "https://chat-app-pearl-six.vercel.app",
+      "https://chat-app-git-main-kunal-giri-s-projects.vercel.app"
     ],
     credentials: true
   }
@@ -57,14 +58,16 @@ io.on("connection", (socket) => {
 app.use(express.json({ limit: "4mb" }));
 
 app.use(
-    cors({
-        origin: [
-            "http://localhost:5173",
-            "https://chat-app-pearl-six.vercel.app"
-        ],
-        credentials: true
-    })
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://chat-app-pearl-six.vercel.app",
+      "https://chat-app-git-main-kunal-giri-s-projects.vercel.app"
+    ],
+    credentials: true
+  })
 );
+
 
 // Import routes
 import userRouter from "./routes/userRoutes.js";
